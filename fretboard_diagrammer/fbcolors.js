fff5c95dae82704aa4a2311aa493f560c7a8007d
mike-hoegeman <mike.hoegeman@gmail.com>
2024-06-20 10:41:21 -0700

    most of the color cfg handling is complete

--- fretboard_diagrammer/fbcolors.js
+++ fretboard_diagrammer/fbcolors.js
@@ -4,6 +4,10 @@
  * elements
  */
 
+/*
+ * Color selector
+ */
+var FretboardColorsPicker = null;
 class FretboardColors {
 
     constructor(fretboard) {
@@ -20,17 +24,16 @@ class FretboardColors {
         this.clrtransparentbackground();
         //
         this.clrtargets()
+    }
 
-        /*
-        this.kreadwritefretboard();
-        this.kstringheader();
-        this.kopenstrings();
-        this.strings = this.kstrings();
-        this.fbcStrings(this.strings, 12);
-        document.getElementById('numstrings').value=12;
-        this.kleftmarkers();
-        this.krightmarkers();
-        */
+    getKeys(obj) {
+        var keys = [];
+        for(var key in obj) {
+            if(obj.hasOwnProperty(key)) {
+                keys.push(key);
+            }
+        }
+        return keys.sort();
     }
 
     clrheader() {
@@ -72,6 +75,29 @@ class FretboardColors {
             { class: 'fbcolors-targets-grid-col-color-hex' });
     }
 
+    updateTargetFromButton(event, colortarget) {
+        const elem = event.target;
+        if (this.fretboard.isIOS) {
+            this.bell();
+            return;
+        }
+
+        // get the target element and change the title
+        // and background-xxxx style
+        if (event.detail.color === '') {
+            this.bell();
+            return;
+        }
+        const color = event.detail.color.toHexString();
+        // save in configuration
+        this.fretboard.cfg.color[colortarget] = color;
+        // update button color
+        elem.style['background-color'] = color;
+        this.fretboard.erase();
+        this.fretboard.draw();
+        this.updateColorWidgets();
+    }
+
     clrtargets() {
         const p = document.getElementsByClassName('clrtargets')[0]; 
 
@@ -80,35 +106,93 @@ class FretboardColors {
             id: 'fbcolors-targets-grid' 
         });
         p.appendChild(hgrid);
+
+        //----
+        // Make the label/heading row
         this.clrtargetsRowDivs(hgrid);
         var l;
         var hx = 0;
         l = this.mk.elemWithAttrs('label', hgrid.children[hx++], { });
-        l.innerHTML="For";
+        l.innerHTML="For element";
         l = this.mk.elemWithAttrs('label', hgrid.children[hx++], { });
-        l.innerHTML="Use color";
+        l.innerHTML="use color";
         l = this.mk.elemWithAttrs('label', hgrid.children[hx++], { });
-        l.innerHTML="With hex value";
+        l.innerHTML="with hex value";
+        //----
 
 
+        //----
+        // widget row for each target
         const fbc = this.fretboard.cfg.color;
-        for (const key in fbc) {
-            if (!fbc.hasOwnProperty(key)) { continue; }
-
+        const keys = this.getKeys(fbc);
+        const buttons = [];
+        for (let i=0; i<keys.length; i++) {
+            var key = keys[i];
             this.clrtargetsRowDivs(hgrid);
 
-            l = this.mk.elemWithAttrs('label', hgrid.children[hx++], { });
-            l.innerHTML=key;
-            this.mk.elemWithAttrs('button', hgrid.children[hx++], { 
-                style: "background-color: " + fbc[key]
+            var e;
+            e = this.mk.elemWithAttrs('label', hgrid.children[hx++], {});
+
+            e = this.mk.elemWithAttrs('button', hgrid.children[hx++], {
+                id: "colortargetbutton"+i
             });
-            this.mk.elemWithAttrs('input', hgrid.children[hx++], { 
+            buttons.push(e);
+
+            e = this.mk.elemWithAttrs('input', hgrid.children[hx++], { 
                 type: 'text',
                 min: 7, max: 7, maxlength:7, size: 7, 
                 value: fbc[key]
             });
         }
+        //----
+        // make color selector
+        if (!this.fretboard.isIOS) {
+            FretboardColorsPicker = Spectrum.createMultiple(buttons, {
+                type: 'color',
+                showPaletteOnly: true,
+                togglePaletteOnly: true,
+                maxSelectionSize: 9,
+                hideAfterPaletteSelect: true,
+                showButtons: true,
+                showAlpha: false,
+                chooseText: "Done",
+                togglePaletteMoreText: "more <click>, <esc> done",
+                palette: LeCorbusierPalette,
+            });
+            //----
+            // bind buttons to selector
+            for (let i=0; i<keys.length; i++) {
+                buttons[i].addEventListener('move', (event) => {
+                    this.updateTargetFromButton(event, keys[i]);
+                });
+            }
+        }
+        //----
+        // update the widget data/appearances for each target row
+        this.updateColorWidgets();
+    }
 
+    updateColorWidgets() {
+        const hgrid = document.getElementById('fbcolors-targets-grid'); 
+        var hx = 3; // skip past header row
+        var e = null;
+
+        const fbc = this.fretboard.cfg.color;
+        const keys = this.getKeys(fbc);
+        for (let i=0; i<keys.length; i++) {
+            var key = keys[i];
+            // label
+            e = hgrid.children[hx++].children[0];
+            e.innerHTML = key;
+
+            // button
+            e = hgrid.children[hx++].children[0];
+            e.style['background-color'] = fbc[key];
+
+            // text input
+            e = hgrid.children[hx++].children[0];
+            e.value = fbc[key];
+        }
     }
 
     bell() {
